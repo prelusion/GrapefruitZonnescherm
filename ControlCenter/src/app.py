0d7a517069eed3cc8aea7b9d36c59b19b192c70c
Rick Huizing <rickhuizing01@gmail.com>
2019-10-29 12:56:02 +0100

    made mainwindow

--- ControlCenter/src/app.py
+++ ControlCenter/src/app.py
@@ -2,43 +2,31 @@ import wx
 from pubsub import pub
 
 
-EVENT_MONEY_CHANGED = "money_changed"
-EVENT_CHANGE_MONEY = "change_money"
-
-
 class MainView(wx.Frame):
     def __init__(self, title):
-        super().__init__(None, title=title, pos=(150, 150), size=(350, 200))
+        super().__init__(None, title=title, size=(1200, 700))
         self.Bind(wx.EVT_CLOSE, self.OnClose)
 
-        menuBar = wx.MenuBar()
-        menu = wx.Menu()
-        m_exit = menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
-        self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
-        menuBar.Append(menu, "&File")
-        self.SetMenuBar(menuBar)
+        self.SetIcon(wx.Icon("Assets/Icons/logo.ico"))
 
-        self.statusbar = self.CreateStatusBar()
+        mainpanel = wx.Panel(self)
+        grid = wx.GridSizer(0, 2, 0, 0)
+        mainpanel.SetSizer(grid)
+        mainpanel.Layout()
 
-        panel = wx.Panel(self)
-        box = wx.BoxSizer(wx.VERTICAL)
+        cupanel = wx.Panel(mainpanel)
+        cupanel.SetBackgroundColour((1, 1, 1))
 
-        m_text = wx.StaticText(panel, -1, "Hello World!")
-        m_text.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
-        m_text.SetSize(m_text.GetBestSize())
-        box.Add(m_text, 0, wx.ALL, 10)
+        datapanel = wx.Panel(mainpanel)
+        datapanel.SetBackgroundColour((100, 100, 100))
 
-        m_close = wx.Button(panel, wx.ID_CLOSE, "Close")
-        m_close.Bind(wx.EVT_BUTTON, self.OnClose)
-        box.Add(m_close, 0, wx.ALL, 10)
-
-        panel.SetSizer(box)
-        panel.Layout()
+        grid.Add(cupanel , 0, wx.EXPAND, 0)
+        grid.Add(datapanel, 0, wx.EXPAND, 0)
 
     def OnClose(self, event):
         dlg = wx.MessageDialog(self,
                                "Do you really want to close this application?",
-                               "Confirm Exit", wx.OK | wx.CANCEL | wx.ICON_QUESTION)
+                               "Confirm Exit", wx.OK | wx.CANCEL)
         result = dlg.ShowModal()
         dlg.Destroy()
         if result == wx.ID_OK:
@@ -47,9 +35,10 @@ class MainView(wx.Frame):
 
 def mainloop():
     app = wx.App(False)
-    mainview = MainView("My title")
+    mainview = MainView("Grapefruit control panel")
     mainview.Show()
     app.MainLoop()
+    
 
 
 if __name__ == "__main__":
