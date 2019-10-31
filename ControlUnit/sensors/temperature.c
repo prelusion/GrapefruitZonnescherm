28128e60594e81622d761fa48fd127cf0dcaa780
Prelusion <d.g.d.van.der.waal@st.hanze.nl>
2019-10-31 12:00:12 +0100

    Review temperature c fixed

--- ControlUnit/sensors/temperature.c
+++ ControlUnit/sensors/temperature.c
@@ -3,14 +3,14 @@
 int8_t get_temperature(void)
 {
 	//calculates the correct temperature with the following formula
-	uint8_t temperature = ((adc_read(PINC0)*(5.0/1024.0))-0.5)*100;
+	int8_t temperature = ((adc_read(PINC0)*(5.0/1024.0))-0.5)*100;
 	return temperature;
 }
 
 //If the temperature is not within the threshold return 0 else return 1
 uint8_t temperature_sensor_connected(void)
 {
-	uint8_t temperature = get_temperature();
+	int8_t temperature = get_temperature();
 	if (temperature > 63 || temperature < -64)
 	{
 		return 0;
