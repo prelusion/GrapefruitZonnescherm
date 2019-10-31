1240ada50520d76ead4db1b04104ae478bd32112
Prelusion <d.g.d.van.der.waal@st.hanze.nl>
2019-10-31 11:57:13 +0100

    Fixed PR review #2

--- ControlUnit/sensors/light_intensity.c
+++ ControlUnit/sensors/light_intensity.c
@@ -1,16 +1,17 @@
 #include <avr/io.h>
 
+const uint16_t MINIMUM_LIGHT_INTENSITY = 0;
+const uint16_t MAXIMUM_LIGHT_INTENSITY = 800;
+
 // Gets the light_intensity of the arduino and converts it to a %amount of light.
 uint8_t get_light_intensity(void)
 {
-	uint16_t min = 0;
-	uint16_t max = 800;
 	//Gets the light_level of the assigned analog pin
 	uint32_t light_level = adc_read(PINC1);
 	//Converts the light intensity to a %amount
-	uint32_t light_intensity = (((light_level - min) * 100) / (max - min));
+	uint32_t light_intensity = (((light_level - MINIMUM_LIGHT_INTENSITY) * 100) / (MAXIMUM_LIGHT_INTENSITY - MINIMUM_LIGHT_INTENSITY));
 	//If the light_level is above the max amount it can handle set the %amount to 100%
-	if(light_level > max)
+	if(light_level > MAXIMUM_LIGHT_INTENSITY)
 	{
 		light_intensity = 100;
 	}
--- ControlUnit/sensors/temperature.c
+++ ControlUnit/sensors/temperature.c
@@ -2,17 +2,15 @@
 
 int8_t get_temperature(void)
 {
-	uint8_t temperature;
 	//calculates the correct temperature with the following formula
-	temperature = ((adc_read(PINC0)*(5.0/1024.0))-0.5)*100;
+	uint8_t temperature = ((adc_read(PINC0)*(5.0/1024.0))-0.5)*100;
 	return temperature;
 }
 
 //If the temperature is not within the threshold return 0 else return 1
 uint8_t temperature_sensor_connected(void)
 {
-	uint8_t temperature;
-	temperature = get_temperature();
+	uint8_t temperature = get_temperature();
 	if (temperature > 63 || temperature < -64)
 	{
 		return 0;
