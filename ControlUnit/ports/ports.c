fc6f17ac3708685e7d96da00868de2babc918a77
Prelusion <d.g.d.van.der.waal@st.hanze.nl>
2019-11-01 12:55:50 +0100

    Main distance core

--- ControlUnit/ports/ports.c
+++ ControlUnit/ports/ports.c
@@ -3,6 +3,11 @@
 //Sets I/O on the right pins
 void init_ports()
 {
-	DDRD = 0b11100000;
-	PINB = 0b11000000; 
+	DDRB = 0b00000111;
+	PORTB = 0b00000111;
+	
+	//Set all pins except pin 7 to output
+	DDRD = 0b01000000;
+	//Disables all pins
+	PORTD = 0x00;
 }
