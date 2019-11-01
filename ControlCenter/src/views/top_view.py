b2f2ae635700deb6e12fcd82637c11ff86a7c749
Jarno Jellesma <jarnojellesma@gmail.com>
2019-11-01 12:58:13 +0100

    diff bug

--- ControlCenter/src/views/top_view.py
+++ ControlCenter/src/views/top_view.py
@@ -14,22 +14,33 @@ class TopView(mvc.View):
         self.SetBackgroundColour((255, 0, 0))
 
         main_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        panel = wx.Panel(self)
+        panel.SetBackgroundColour((100, 100, 100))
+
+        main_sizer.Add(panel, wx.ID_ANY, wx.EXPAND | wx.ALL)
+
         self.SetSizer(main_sizer)
-
-        # Left panel
-        left_panel = wx.Panel(self)
-        left_panel_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        left_panel.SetSizer(left_panel_sizer)
-        left_panel.SetBackgroundColour((0, 255, 0))
-
-        # Right panel
-        right_panel = wx.Panel(self)
-        right_panel_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        right_panel.SetSizer(right_panel_sizer)
-        right_panel.SetBackgroundColour((0, 0, 255))
-
-        main_sizer.Add(left_panel, wx.ID_ANY, wx.EXPAND | wx.ALL)
-        main_sizer.Add(right_panel, wx.ID_ANY, wx.EXPAND | wx.ALL)
 
         main_sizer.Layout()
         self.Layout()
+
+        # panel_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        # Left panel
+        # left_panel = wx.Panel(self)
+        # left_panel_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        # left_panel.SetSizer(left_panel_sizer)
+        # left_panel.SetBackgroundColour((0, 255, 0))
+        #
+        # # Right panel
+        # right_panel = wx.Panel(self)
+        # right_panel_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        # right_panel.SetSizer(right_panel_sizer)
+        # right_panel.SetBackgroundColour((0, 0, 255))
+        #
+        # panel_sizer.Add(left_panel, wx.ID_ANY, wx.EXPAND | wx.ALL)
+        # panel_sizer.Add(right_panel, wx.ID_ANY, wx.EXPAND | wx.ALL)
+
+
+
