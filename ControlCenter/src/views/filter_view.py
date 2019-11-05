57f5481da99c07b585219466884754b1fa6897ce
Rick Huizing <rickhuizing01@gmail.com>
2019-11-05 15:36:21 +0100

    add filterlabel panel to filterview

--- ControlCenter/src/views/filter_view.py
+++ ControlCenter/src/views/filter_view.py
@@ -1,6 +1,7 @@
 import wx
 
 from src import mvc
+from src import widgets
 
 
 class FilterView(mvc.View):
@@ -14,15 +15,32 @@ class FilterView(mvc.View):
         super().__init__(parent)
         self.SetBackgroundColour((255, 255, 255))
 
-        self.gridsizer = wx.GridSizer(2, 2, 5, 5)
-        self.SetWindowStyle(wx.BORDER_SIMPLE)
+        main_sizer = wx.GridSizer(2, 1, 0, 0)
+        self.SetSizer(main_sizer)
 
+        title_panel = wx.Panel(self)
+        h_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        title_panel.SetSizer(h_sizer)
+        label = widgets.CenteredLabel(title_panel, "Filters")
+        label.SetFont(wx.Font(12, wx.DECORATIVE, wx.NORMAL, wx.BOLD))
+        h_sizer.Add(label, wx.ID_ANY, wx.EXPAND | wx.ALL )
+
+
+        checkbox_panel = wx.Panel(self)
+        checkbox_panel.SetBackgroundColour((255, 255, 255))
+
+        self.gridsizer = wx.GridSizer(2, 2, 5, 5)
         self.checkboxes = {}
 
         for checkbox in (self.CHECKBOX_CONNECTED, self.CHECKBOX_STATUS_UP,
                          self.CHECKBOX_SELECT_ALL, self.CHECKBOX_STATUS_DOWN):
 
-            self.checkboxes[checkbox] = wx.CheckBox(self, label=checkbox)
+            self.checkboxes[checkbox] = wx.CheckBox(checkbox_panel, label=checkbox)
             self.gridsizer.Add(self.checkboxes[checkbox], 0, wx.EXPAND)
 
-        self.SetSizer(self.gridsizer)
+        checkbox_panel.SetSizer(self.gridsizer)
+
+        main_sizer.Add(title_panel,  wx.ID_ANY, wx.EXPAND | wx.ALL)
+        main_sizer.Add(checkbox_panel,  wx.ID_ANY, wx.EXPAND | wx.ALL)
+
+        self.SetWindowStyle(wx.BORDER_SIMPLE)
